ef150976e86f4e63de71f094e1f261a3a194a2eb
jarzuaga94 <jarzuaga@comcast.net>
2015-03-25 15:51:16 -0400

    string, permissions, and layout files done

--- app/src/main/java/cs/uml/edu/projectsilence/MainScreen.java
+++ app/src/main/java/cs/uml/edu/projectsilence/MainScreen.java
@@ -1,19 +1,44 @@
 package cs.uml.edu.projectsilence;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
-public class MainScreen extends ActionBarActivity {
+public class MainScreen extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_screen);
-    }
+        // Create a new TodoListAdapter for this ListActivity's ListView
+        //mAdapter = new ToDoListAdapter(getApplicationContext());
 
+        // Put divider between ToDoItems and FooterView
+        getListView().setFooterDividersEnabled(true);
+
+        //TODO - Inflate footerView for footer_view.xml file
+        TextView footerView = (TextView) this.getLayoutInflater().inflate(R.layout.footer, null);
+
+        //TODO - Add footerView to ListView
+        getListView().addFooterView(footerView);
+        footerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //TODO - Attach Listener to FooterView. Implement onClick().
+                //Intent intent = new Intent(getBaseContext(), AddEvent.class);
+                //startActivityForResult(intent, ADD_EVENT_R);
+
+            }
+        });
+
+        //TODO - Attach the adapter to this ListActivity's ListView
+        //getListView().setAdapter(mAdapter);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
