0435d66ebb799e9bd39c3b55c57f761e4f8d3cf9
jarzuaga94 <jarzuaga@comcast.net>
2015-04-15 14:46:32 -0400

    alarm set for endTime

--- app/src/main/java/cs/uml/edu/projectsilence/AlarmReceiver.java
+++ app/src/main/java/cs/uml/edu/projectsilence/AlarmReceiver.java
@@ -3,6 +3,7 @@ package cs.uml.edu.projectsilence;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.os.Vibrator;
 import android.telephony.SmsManager;
 import android.widget.Toast;
@@ -13,27 +14,38 @@ import android.widget.Toast;
 public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent){
-
-        System.out.println("enter broadcast receiver");
-        /*String phoneNumberReciver="9718202185";// phone number to which SMS to be send
-        String message="Hi I will be there later, See You soon";// message to send
-        SmsManager sms = SmsManager.getDefault();
-        sms.sendTextMessage(phoneNumberReciver, null, message, null, null);
-        // Show the toast  like in above screen shot*/
-
-        String phoneNumber = "16034758425";
-        String message = "Test";
-
-        sendSMS( phoneNumber, message);
-
-        Vibrator v;
-        v=(Vibrator)context.getSystemService(Context.VIBRATOR_SERVICE);
-        v.vibrate(2000);
-        Toast.makeText(context, "Alarm Triggered and SMS Sent", Toast.LENGTH_LONG).show();
-
-
-
-    }
+        boolean muteSound = intent.getBooleanExtra(EventItem.MUTE_SOUND, false);
+        boolean sendMessage = intent.getBooleanExtra(EventItem.SEND_TEXT, false);
+        boolean isStartAlarm = intent.getBooleanExtra(MainScreen.isStartAlarm, false);
+        Toast.makeText(context, "Alarm receiver", Toast.LENGTH_LONG).show();
+        if(isStartAlarm){
+            if (muteSound) {
+                AudioManager amanager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+                amanager.setStreamMute(AudioManager.STREAM_NOTIFICATION, true);
+                amanager.setStreamMute(AudioManager.STREAM_ALARM, true);
+                amanager.setStreamMute(AudioManager.STREAM_MUSIC, true);
+                amanager.setStreamMute(AudioManager.STREAM_RING, true);
+                amanager.setStreamMute(AudioManager.STREAM_SYSTEM, true);
+            }
+            if(sendMessage){
+                String phoneNumber = "16034758425";
+                String message = "Test";
+                sendSMS( phoneNumber, message);
+                Vibrator v;
+                v=(Vibrator)context.getSystemService(Context.VIBRATOR_SERVICE);
+                v.vibrate(2000);
+                Toast.makeText(context, "Alarm Triggered and SMS Sent", Toast.LENGTH_LONG).show();
+            }
+        }
+        else {
+            AudioManager amanager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+            amanager.setStreamMute(AudioManager.STREAM_NOTIFICATION, false);
+            amanager.setStreamMute(AudioManager.STREAM_ALARM, false);
+            amanager.setStreamMute(AudioManager.STREAM_MUSIC, false);
+            amanager.setStreamMute(AudioManager.STREAM_RING, false);
+            amanager.setStreamMute(AudioManager.STREAM_SYSTEM, false);
+        }
+      }
 
     private void sendSMS(String phoneNumber, String message)
     {
